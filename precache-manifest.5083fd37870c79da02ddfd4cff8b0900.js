5eea6d3eec3f2c37c79f2c65c8932cb726b03ad3
iamsanga <iamsanga7@gmail.com>
2020-04-10 22:46:26 +0530

    Updates

---
+++ precache-manifest.5083fd37870c79da02ddfd4cff8b0900.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ef8e48cad93fb9d4a25e47cd5fbdeb1",
+    "url": "/react-todo-app/index.html"
+  },
+  {
+    "revision": "471f0f6a176b0145192a",
+    "url": "/react-todo-app/static/css/main.f9fffb7b.chunk.css"
+  },
+  {
+    "revision": "bf961796e6c3c411f519",
+    "url": "/react-todo-app/static/js/2.c5e4b31f.chunk.js"
+  },
+  {
+    "revision": "471f0f6a176b0145192a",
+    "url": "/react-todo-app/static/js/main.b4ffae8d.chunk.js"
+  },
+  {
+    "revision": "b1aeed9f6344163e5e40",
+    "url": "/react-todo-app/static/js/runtime~main.a3a648b2.js"
+  }
+]);
